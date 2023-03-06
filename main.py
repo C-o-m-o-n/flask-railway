2029cf780868d6f1de70cea4b4537d90583146b9
C-o-m-o-n <comon928@gmail.com>
2023-03-06 12:53:53 +0300

    first commit

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, redirect, request
 import os
-
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///database.db'
+app.config['SECRET_KEY']='sqlite:///database'
 
-@app.route('/')
+db = SQLAlchemy(app)
+
+#define the poems table
+class Poems(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  date_posted = db.Column(db.DateTime)
+  content = db.Column(db.String(200))
+  
+#with app.app_context:
+@app.route('/', methods=['POST','GET'])
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
+  if request.method == 'POST':
+    poem_content = request.form['content']
+    
+    poem= Poems(date_posted=datetime.now(), content=poem_content)
+    db.create_all()
+    db.session.add(poem)
+    db.session.commit()
+    #print('added successfully')
+  poems = Poems.query.all()
+  return render_template('index.html', poems=poems)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True)
