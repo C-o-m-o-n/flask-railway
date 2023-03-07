8fb6422b91dde915e7b9a7980f33733f4113a9f0
C-o-m-o-n <comon928@gmail.com>
2023-03-07 04:09:36 +0300

    fdpmt

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask, jsonify, render_template, redirect, request
+from flask import Flask, jsonify, render_template, redirect, request, url_for
 import os
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime, time
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///database.db'
@@ -13,21 +13,45 @@ db = SQLAlchemy(app)
 class Poems(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   date_posted = db.Column(db.DateTime)
+  title = db.Column(db.String(200))
+  author = db.Column(db.String(200))
   content = db.Column(db.String(200))
   
 #with app.app_context:
-@app.route('/', methods=['POST','GET'])
+@app.route('/')
 def index():
+  poems = Poems.query.all()
+  return render_template('index.html', poems=poems)
+  
+#add poem
+@app.route("/addpoem", methods=['POST', 'GET'])
+def addpoem():
   if request.method == 'POST':
+    poem_title = request.form['title']
     poem_content = request.form['content']
-    
-    poem= Poems(date_posted=datetime.now(), content=poem_content)
+    poem_author = request.form['author']
+    poem= Poems(date_posted=datetime.now(), title=poem_title, content=poem_content, author=poem_author)
     db.create_all()
     db.session.add(poem)
     db.session.commit()
-    #print('added successfully')
-  poems = Poems.query.all()
-  return render_template('index.html', poems=poems)
+    return redirect(url_for('index'))
+  else:
+    print('error')
+  return render_template('addpoem.html')
+  
+@app.route('/poem/<int:poem_id>')
+def poem(poem_id):
+  poem = Poems.query.filter_by(id=poem_id).first()
+  return render_template('poem.html', poem=poem)
+  
+  
+@app.route('/delete/<int:poem_id>')
+def delete(poem_id):
+  poem_del = Poems.query.filter_by(id=poem_id).first()
+  db.session.delete(poem_del)
+  db.session.commit()
+  return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
